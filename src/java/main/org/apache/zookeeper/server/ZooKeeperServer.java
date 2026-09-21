c00b48c9d6f8b095125b4865af76568c623cf7cd
Cheng TANG <tcsunrise07@gmail.com>
2026-09-21 23:46:48 +0800

    docs: 整理 ZooKeeperServer 代码格式

--- src/java/main/org/apache/zookeeper/server/ZooKeeperServer.java
+++ src/java/main/org/apache/zookeeper/server/ZooKeeperServer.java
@@ -386,8 +386,7 @@ public class ZooKeeperServer implements SessionExpirer, ServerStats.Provider {
 
     protected void setupRequestProcessors() {
         RequestProcessor finalProcessor = new FinalRequestProcessor(this);
-        RequestProcessor syncProcessor = new SyncRequestProcessor(this,
-                finalProcessor);
+        RequestProcessor syncProcessor = new SyncRequestProcessor(this, finalProcessor);
         ((SyncRequestProcessor)syncProcessor).start();
         firstProcessor = new PrepRequestProcessor(this, syncProcessor);
         ((PrepRequestProcessor)firstProcessor).start();
